63d1dcda3f7fad5bc6f937e32f2e9745f52d5d26
NevandaMeilyna <meilynanevanda4@gmail.com>
2025-06-24 12:29:13 +0700

    import vue router

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css' //import bootstrap
+import router from './router' //import vue router
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
 
